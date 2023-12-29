e4dfb5dbd9661c5977593868f8efbb75cc3be57e
Kyle Yan <kyleyankc@gmail.com>
2023-12-29 09:27:14 -0500

    Improve navbar styling

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,38 +5,65 @@ import ThemeSwitch from "./ThemeSwitch";
 const NavBar = () => {
   return (
     <header className="flex p-6 z-50 items-center justify-between">
-      <Link href="/" className="text-xl font-light border-0">
-        Skyward
-      </Link>
       <div className="flex space-x-4 items-center">
-        <nav className="flex space-x-4 items-center sm:space-x-6">
-          <ThemeSwitch />
-          <Link href="/" className="inline-block border-0 pb-0">
+        <Link href="/" className="text-xl font-light border-0">
+          Skyward
+        </Link>
+        <ThemeSwitch />
+      </div>
+      <nav className="flex items-center space-x-6">
+        <Link href="/" className="inline-block border-0 pb-0">
+          <div className="flex space-x-2">
             <Home
               strokeWidth="1"
               className="stroke-gray-600 hover:stroke-gray-900
                            dark:stroke-gray-400 dark:hover:stroke-gray-200
                              transition duration-300"
             />
-          </Link>
-          <Link href="/about" className="inline-block border-0 pb-0">
+            <span
+              className="text-gray-600 hover:text-gray-900
+                           dark:text-gray-400 dark:hover:text-gray-200
+                             transition duration-300 hidden sm:inline-flex"
+            >
+              Home
+            </span>
+          </div>
+        </Link>
+        <Link href="/about" className="inline-block border-0 pb-0">
+          <div className="flex space-x-2">
             <Smile
               strokeWidth="1"
               className="stroke-gray-600 hover:stroke-gray-900
                            dark:stroke-gray-400 dark:hover:stroke-gray-200
                              transition duration-300"
             />
-          </Link>
-          <Link href="/now" className="inline-block border-0 pb-0">
+            <span
+              className="text-gray-600 hover:text-gray-900
+                           dark:text-gray-400 dark:hover:text-gray-200
+                             transition duration-300 hidden sm:inline-flex"
+            >
+              About
+            </span>
+          </div>
+        </Link>
+        <Link href="/now" className="inline-block border-0 pb-0">
+          <div className="flex space-x-2">
             <Headphones
               strokeWidth="1"
               className="stroke-gray-600 hover:stroke-gray-900
                            dark:stroke-gray-400 dark:hover:stroke-gray-200
                              transition duration-300"
             />
-          </Link>
-        </nav>
-      </div>
+            <span
+              className="text-gray-600 hover:text-gray-900
+                           dark:text-gray-400 dark:hover:text-gray-200
+                             transition duration-300 hidden sm:inline-flex"
+            >
+              Now
+            </span>
+          </div>
+        </Link>
+      </nav>
     </header>
   );
 };
