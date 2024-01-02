9dadcf14593ebeef865eafcda3a65b0dcb461a83
Kyle Yan <kyleyankc@gmail.com>
2024-01-02 12:45:45 -0800

    Fix RSS feed

--- app/feed/route.ts
+++ app/feed/route.ts
@@ -7,19 +7,21 @@ export async function GET() {
   const feed = new Rss({
     title: "Skyward",
     description: "Beyond awesome.",
-    feed_url: `${SITE_URL}/rss`,
+    feed_url: `${SITE_URL}/feed`,
     site_url: SITE_URL,
     language: "en",
   });
 
-  allPosts.forEach((post) => {
-    feed.item({
-      title: post.title,
-      description: post.summary ?? "",
-      url: `${SITE_URL}/${post.slug}`,
-      date: post.date,
+  allPosts
+    .filter((post) => !post.draft)
+    .forEach((post) => {
+      feed.item({
+        title: post.title,
+        description: post.summary ?? "",
+        url: `${SITE_URL}/${post.slug}`,
+        date: post.date,
+      });
     });
-  });
 
   return new Response(feed.xml(), {
     headers: {
