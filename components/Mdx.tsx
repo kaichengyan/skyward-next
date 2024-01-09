3d5fda7975de3b0b7e85af9cc24826fe058eb7d0
Kyle Yan <kyleyankc@gmail.com>
2024-01-08 22:41:56 -0800

    Support embedding tweets

--- components/Mdx.tsx
+++ components/Mdx.tsx
@@ -2,10 +2,16 @@ import FullWidthImage from "@/components/FullWidthImage";
 import SimpleImage from "@/components/SimpleImage";
 import React from "react";
 import { useMDXComponent } from "next-contentlayer/hooks";
+import { Tweet } from "react-tweet";
 
 const components = {
   SimpleImage,
   FullWidthImage,
+  Tweet: (props: any) => (
+    <div className="flex justify-center">
+      <Tweet {...props} />
+    </div>
+  ),
 };
 
 interface MdxProps {
