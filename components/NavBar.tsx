19017a7c14987b20e57c23a7e504db980ef6543c
Kyle Yan <kyleyankc@gmail.com>
2024-01-10 20:32:59 -0800

    Simplify NavBar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,41 +3,32 @@ import ThemeSwitch from "./ThemeSwitch";
 
 export default function NavBar() {
   return (
-    <header className="flex p-6 z-50 items-center justify-between">
+    <header className="font-overpass flex p-6 z-50 items-center justify-between">
       <div className="flex space-x-4 items-center">
-        <Link href="/" className="font-overpass text-xl font-light border-0">
+        <Link href="/" className="text-xl font-light border-0">
           Skyward
         </Link>
       </div>
-      <nav className="flex items-center space-x-4 sm:space-x-8">
-        <Link href="/" className="inline-block border-0 pb-0">
-          <span
-            className="font-overpass text-gray-600 hover:text-gray-900
-                       dark:text-gray-400 dark:hover:text-gray-200
-                       transition duration-300 inline-flex"
-          >
-            Home
-          </span>
+      <nav className="flex items-center space-x-4 sm:space-x-8 text-gray-600 dark:text-gray-400">
+        <Link
+          href="/"
+          className="border-0 pb-0 hover:text-gray-900 dark:hover:text-gray-200 transition duration-300"
+        >
+          Home
         </Link>
-        <Link href="/about" className="inline-block border-0 pb-0">
-          <span
-            className="font-overpass text-gray-600 hover:text-gray-900
-                       dark:text-gray-400 dark:hover:text-gray-200
-                       transition duration-300 inline-flex"
-          >
-            About
-          </span>
+        <Link
+          href="/about"
+          className="border-0 pb-0 hover:text-gray-900 dark:hover:text-gray-200 transition duration-300"
+        >
+          About
         </Link>
-        <Link href="/now" className="inline-block border-0 pb-0">
-          <span
-            className="font-overpass text-gray-600 hover:text-gray-900
-                       dark:text-gray-400 dark:hover:text-gray-200
-                       transition duration-300 inline-flex"
-          >
-            Now
-          </span>
+        <Link
+          href="/now"
+          className="border-0 pb-0 hover:text-gray-900 dark:hover:text-gray-200 transition duration-300"
+        >
+          Now
         </Link>
-        <div className="hidden sm:inline-flex sm:w-11">
+        <div className="hidden sm:inline sm:w-11">
           <ThemeSwitch />
         </div>
       </nav>
