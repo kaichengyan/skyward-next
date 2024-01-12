acadab9192e9364597ab15d59240fae045f6fd11
Kyle Yan <kyleyankc@gmail.com>
2024-01-12 09:53:32 -0800

    Unify banner and TagPill colors

--- components/TagPill.tsx
+++ components/TagPill.tsx
@@ -4,10 +4,9 @@ export default function TagPill({ tagName }: { tagName: string }) {
   return (
     <Link
       href={`/tag/${tagName}`}
-      className="inline-block border-0 no-underline align-middle rounded
-            text-gray-600 bg-gray-200 hover:bg-gray-300
-            dark:text-gray-200 dark:hover:bg-gray-700 dark:bg-gray-800
-            text-xs px-1 py-0.5 whitespace-nowrap"
+      className="inline-block border-0 no-underline align-middle rounded text-xs px-1 py-0.5 whitespace-nowrap
+                bg-gray-100 hover:bg-gray-300 text-gray-600
+                dark:bg-gray-900 dark:hover:bg-gray-700 dark:text-gray-400"
       passHref
     >
       {tagName}
